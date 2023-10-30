3d477c2c1bba9eb483ee4c7b70c8c1b35335ee8b
EMBEDDED <dlsxo1023@naver.com>
2023-10-30 15:00:57 +0900

    20231030

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,7 +3,22 @@
 
 #include <iostream>
 #define SQUARE(x) ((x)*(x)) // 매크로 함수 // 제곱
-#define ABS(x)  (x < 0) ? -(x) : (x) // 매크로 함수 // 삼항 연산자 // 절대값
+#define ABS(x)  (x < 0) ? -(x) : (x) // 매크로 함수 // 삼항 연산자 // 모든 인수에 반드시 괄호 //절대값
+// 전역변수/데이터 타잎 선언
+
+/*typedef struct Point   // 구조체 선언
+{
+    int x;
+    int y;
+}point2D;*/
+
+typedef struct Point 
+{
+    int x;
+    int y;
+}point2D;
+
+double Dist(point2D p1, point2D p2);
 int main()
 {
     printf("안녕하세요. C++ 의 세계에 오신것을 환영합니다!\n\n"); 
@@ -14,12 +29,24 @@ int main()
     printf("%d의 절대값 : %d\n",x , ABS(x));
     printf("-4의 절대값 : %d\n", ABS(-4));
 
-    
+    point2D p1, p2;  // 구조체 두 점 선언
+    p1.x = 10; p1.y = 10;
+    p2.x = 20; p2.y = 30;
 
+    double d = Dist(p1, p2);
+    printf("두 점 p1(%d, %d), p2(%d, %d)의 거리는 : %.2f 입니다", p1.x, p1.y, p2.x, p2.y, d);
 
-    
 }
 
+double Dist(point2D p1, point2D p2) //두 점간의 거리
+{
+    int w = ABS(p1.x - p2.x);
+    int h = ABS(p1.y - p2.y);
+    int w1 = w * w;
+    int h1 = h * h;
+    double d = sqrt(w1 + h1);   //sqrt = 루트 
+    return d;
+}
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
 // 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
