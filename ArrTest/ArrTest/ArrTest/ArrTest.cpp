98f1a612c88821b15b1db8489542345c23d5399d
EMBEDDED <dlsxo1023@naver.com>
2023-11-27 11:00:17 +0900

    202031127

--- ArrTest/ArrTest/ArrTest/ArrTest.cpp
+++ ArrTest/ArrTest/ArrTest/ArrTest.cpp
@@ -14,15 +14,34 @@ public:
 	{
 		delete arr;
 	}
-	int operator[](int idx) // calling sequence : arr[n]
+	int length()
 	{
-		return *(arr + idx);
+		return Len;
+	}
+	int& operator[](int idx) // calling sequence : arr[n]
+	{
+		if (idx < 0 || idx > Len - 1)
+		{
+			std::cout << "Out of bound\n"; exit(1);
+		}
+		//return *(arr + idx);
 		return arr[idx];
 	}
+	ArrTest& append(int size);
 };
+ArrTest& ArrTest::append(int size)
+{
+
+}
 
 int main()
 {
-   
+	int a1[] = { 11, 22, 33, 44, 55 };
+	ArrTest arr(7);
+	for (int i = 0; i < 5; i++)
+		arr[i] = a1[i];
+	for (int i = 0; i < arr.length(); i++)
+		printf("ArrTest[%d] = %d\n",i, arr[i]);
+	int a2[] = { 21, 22, 23, 24, 25 };
 }
 
