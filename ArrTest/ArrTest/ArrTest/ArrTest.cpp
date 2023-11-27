4f28d09316a4ffd3feb291563d60143df4aea75a
EMBEDDED <dlsxo1023@naver.com>
2023-11-27 10:29:56 +0900

    20231127

---
+++ ArrTest/ArrTest/ArrTest/ArrTest.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+class ArrTest
+{
+private:
+	int *arr;
+	int Len;
+public:
+	ArrTest(int size) : Len(size) 
+	{
+		arr = new int[size];
+	}
+	~ArrTest()
+	{
+		delete arr;
+	}
+	int operator[](int idx) // calling sequence : arr[n]
+	{
+		return *(arr + idx);
+		return arr[idx];
+	}
+};
+
+int main()
+{
+   
+}
+
