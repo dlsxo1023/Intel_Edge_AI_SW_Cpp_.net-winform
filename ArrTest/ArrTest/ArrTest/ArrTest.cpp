27c3e05e4a6032c595a45b8b7432323c07c74c31
EMBEDDED <dlsxo1023@naver.com>
2023-11-27 17:56:15 +0900

    20231127

--- ArrTest/ArrTest/ArrTest/ArrTest.cpp
+++ ArrTest/ArrTest/ArrTest/ArrTest.cpp
@@ -1,14 +1,15 @@
 ﻿#include <iostream>
 
+template <typename T>
 class ArrTest
 {
 private:
-	int* arr;
-	int Len;
+	T* arr;
+	int Len;  // 배열 요소의 수 
 public:
 	ArrTest(int size) : Len(size)
 	{
-		arr = new int[size];
+		arr = new T[size];
 	}
 	~ArrTest()
 	{
@@ -18,7 +19,7 @@ public:
 	{
 		return Len;
 	}
-	int& operator[](int idx) // calling sequence : arr[n]
+	T& operator[](int idx) // calling sequence : arr[n]
 	{
 		if (idx < 0 || idx > Len - 1)
 		{
@@ -27,36 +28,60 @@ public:
 		//return *(arr + idx);
 		return arr[idx];
 	}
-	void Show();
-	ArrTest& append(int size);
-	ArrTest& append(ArrTest& brr);
+	void Show()
+	{
+		int i;
+		printf("{ ");
+		for (i = 0; i < Len - 1; i++) 
+			std::cout << arr[i] << ", "; std::cout << arr[i];
+		printf(" }\n");
+	}
+	ArrTest& append(int size)
+	{
+		T* arr1 = new T[Len + size];  //확장된 메모리
+		memcpy(arr1, arr, Len * sizeof(T));
+		delete arr;
+		arr = arr1;
+		Len += size;
+		return *this;
+	}
+	ArrTest& append(ArrTest& brr)
+	{
+		T* arr1 = new T[Len + brr.length()];
+		memcpy(arr1, arr, Len * sizeof(T));
+		memcpy(arr1 + Len, brr.arr, brr.length() * sizeof(T));
+		delete arr;
+		arr = arr1;
+		Len += brr.length();
+		return *this;
+	}
 };
-ArrTest& ArrTest::append(int size)  // size : total ? 추가로 배열을 추가하는 함수 
-{
-	int *arr1 = new int[Len + size];  //확장된 메모리
-	memcpy(arr1, arr, Len * sizeof(int));
-	delete arr;
-	arr = arr1; 
-	Len += size;
-	return *this;
-}
-ArrTest& ArrTest::append(ArrTest& brr) // size : total
-{
-	int* arr1 = new int[Len + brr.length()];
-	memcpy(arr1, arr, Len * sizeof(int));
-	memcpy(arr1 + Len, brr.arr, brr.length() * sizeof(int));
-	delete arr;
-	arr = arr1;
-	Len += brr.length();
-	return *this;
-}
-void ArrTest::Show()
-{
-	int i;
-	printf("{");
-	for (i = 0; i < Len - 1; i++) printf(" %d, ", arr[i]); printf(" %d ", arr[i]);
-	printf("}\n");
-}
+//ArrTest& ArrTest::append(int size)  // size : total ? 추가로 배열을 추가하는 함수 
+//{
+//	int *arr1 = new int[Len + size];  //확장된 메모리
+//	memcpy(arr1, arr, Len * sizeof(int));
+//	delete arr;
+//	arr = arr1; 
+//	Len += size;
+//	return *this;
+//}
+//ArrTest& ArrTest::append(ArrTest& brr) // size : total
+//{
+//	int* arr1 = new int[Len + brr.length()];
+//	memcpy(arr1, arr, Len * sizeof(int));
+//	memcpy(arr1 + Len, brr.arr, brr.length() * sizeof(int));
+//	delete arr;
+//	arr = arr1;
+//	Len += brr.length();
+//	return *this;
+//}
+//void ArrTest::Show()
+//{
+//	int i;
+//	printf("{");
+//	for (i = 0; i < Len - 1; i++) printf(" %d, ", arr[i]); printf(" %d ", arr[i]);
+//	printf("}\n");
+//}
 
 class Func
 {
@@ -71,7 +96,11 @@ public:
 	{
 		return a * b;
 	}
-	ArrTest& operator()(ArrTest& b1, ArrTest& b2)
+	ArrTest<int>& operator()(ArrTest<int>& b1, ArrTest<int>& b2)
+	{
+		return  b1.append(b2);
+	}
+	ArrTest<double>& operator()(ArrTest<double>& b1, ArrTest<double>& b2)
 	{
 		return  b1.append(b2);
 	}
@@ -79,13 +108,13 @@ public:
 
 int main()
 {
-	int a1[] = { 11, 22, 33, 44, 55 };
-	ArrTest arr(5);
+	double a1[] = { 11.1, 22.1, 33.1, 44.1, 55.1 };
+	ArrTest<double> arr(5);
 	for (int i = 0; i < 5; i++)            arr[i] = a1[i];
 	//for (int i = 0; i < arr.length(); i++) printf("ArrTest[%d] = %d\n",i, arr[i]);
 	//
-	int a2[] = { 21, 22, 23, 24, 25 };
-	ArrTest brr(5);
+	double a2[] = { 21.1, 22.1, 23.1, 24.1, 25.1 };
+	ArrTest<double> brr(5);
 	for (int i = 0; i < 5; i++)    brr[i] = a2[i];
 	//
 	//int n = arr.length();
