20938a74b40e15d92c591d4d00c4276a90cc3b10
EMBEDDED <dlsxo1023@naver.com>
2023-11-27 16:51:10 +0900

    20231127

---
+++ TemplateTest/TemplateTest/TemplateTest.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+template <class T>
+void Swap(T &a, T &b)
+{
+    T c = a;
+    a = b; 
+    b = c;
+}
+
+int main()
+{
+    int a = 10, b = 20;
+    printf("a = %d  b = %d\n", a, b);
+    Swap(a, b);
+    printf("a = %d  b = %d\n", a, b);
+    
+    double a1 = 2.5, b1 = 3.6;
+    printf("a1 = %.1f  b1 = %.1f\n", a1, b1);
+    Swap(a1, b1);
+    printf("a1 = %.1f  b1 = %.1f\n", a1, b1);
+}
