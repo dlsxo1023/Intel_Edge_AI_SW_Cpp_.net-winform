dd7d2b7b53e2646c8a9ce6057973395c843c5b93
EMBEDDED <dlsxo1023@naver.com>
2024-01-04 17:24:59 +0900

    20240104

--- WinApp/mfcMemo/resource.h
+++ WinApp/mfcMemo/resource.h
@@ -12,7 +12,6 @@
 #define IDR_ACCEL1                      134
 #define IDC_EDIT1                       1000
 #define IDC_EDIT_MEMO                   1000
-#define IDC_EDIT_REPLACE                1000
 #define IDC_EDIT3                       1002
 #define IDC_EDIT_FIND                   1002
 #define IDC_COMBO1                      1003
@@ -44,9 +43,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         32797
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
